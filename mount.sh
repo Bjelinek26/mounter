029beb096cd9af048acb949332fd8553c61191e0
bjelinek26 <brad.jelinek.226@gmail.com>
2018-11-13 12:32:33 -0500

    On branch while_loop  Changes to be committed: 	modified:   mount.sh
    Added a While loop so that after displaying blkid output the user is
    again asked for the file path or for blkid

--- mount.sh
+++ mount.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-read -p "This script requires the full path of the device you wish to mount
-as well as the mount point.  If you know this information enter 1  If you 
-would like some output to help you find this information press 2 " ANS
+while [ "$ANS" != 1 ]
+do
 
-case $ANS in
-	1) 
-    read -p "full path to device: " DEVICE
-    read -p "full path to mount point " MPOINT
+	read -p "This script requires the full path of the device you wish to mount
+	as well as the mount point.  If you know this information enter 1  If you 
+	would like some output to help you find this information press 2 " ANS
 
-    sudo mount $DEVICE $MPOINT
 
-    ;;
-    2)
-	sudo blkid
-	;;
-esac
+	case $ANS in
+		1) 
+    	read -p "full path to device: " DEVICE
+    	read -p "full path to mount point " MPOINT
+
+    	sudo mount $DEVICE $MPOINT
+
+    	;;
+    	2)
+		sudo blkid
+		;;
+	esac
+done
